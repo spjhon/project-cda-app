3f89775ce09a01762ffa4d703e7bd16eda586c5b
PC Escritorio <spjhon@gmail.com>
2026-08-08 14:48:30 -0500

    se mejoran las opcioens del siganture

--- src/components/dashboard/recepcionista/SignatureSection.tsx
+++ src/components/dashboard/recepcionista/SignatureSection.tsx
@@ -385,7 +385,7 @@ function SignatureCardItem({
                   >
                     {size.width > 0 && size.height > 0 && (
                       <Signature
-                        options={{ size: 5, thinning: 0, smoothing: 0, streamline: 0, simulatePressure: false }}
+                        options={{ size: 3, thinning: 0, smoothing: 0, streamline: 0.5, simulatePressure: false }}
                         ref={canvasRef}
                         width={size.width}
                         height={size.height}
