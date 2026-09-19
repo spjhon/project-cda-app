fef503be62b8f2e12bc3e9433276842da2e783a2
PC Escritorio <spjhon@gmail.com>
2026-09-18 20:09:16 -0500

    se organiza mejor el proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,18 +1,21 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/lib/supabase/proxy";
-
-export async function proxy(request: NextRequest) {
-  return await updateSession(request);
-}
-
 export const config = {
   matcher: [
-    /*
-     * Excluimos explícitamente:
-     * - _next/static, _next/image
-     * - Archivos estáticos comunes (imágenes, fuentes, favicons, json, txt)
-     * - Ataques de bots (.php, wp-, .env)
-     */
-    "/((?!$|about$|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp|woff|woff2|ttf|eot|ico|json)$|.*\\.php$|wp-.*|\\.env).*)",
+    // Landing de los tenants:
+    // fullmotos.cda-app.com/
+    // otro.cda-app.com/
+    {
+      source: "/",
+      has: [
+        {
+          type: "header",
+          key: "host",
+          value: "^(?!cda-app\\.com$)[^.]+\\.cda-app\\.com$",
+        },
+      ],
+    },
+
+    // Todo lo demás, excepto la raíz "/"
+    // Esto evita que cda-app.com/ ejecute Proxy.
+    "/((?!$|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.(?:svg|png|jpg|jpeg|gif|webp|woff|woff2|ttf|eot|ico|json)$|.*\\.php$|wp-.*|\\.env).*)",
   ],
 };
